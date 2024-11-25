510602a9e6ae610e8fd06787f0e67d1618f6e309
Tiago Antoniel <tiagofull7@gmail.com>
2024-11-25 19:37:26 -0300

    Commit exercicio9 int pra Wrapper

---
+++ src/exercicio9/Tarefa_Wrapper.java
@@ -0,0 +1,11 @@
+package exercicio9;
+
+public class Tarefa_Wrapper {
+    public static void main(String[] args) {
+        int numeroPrimitivo = 10;
+        Integer numeroWrapper = Integer.valueOf(numeroPrimitivo);
+
+        System.out.println("ValueOf: " + numeroPrimitivo);
+    }
+
+}
